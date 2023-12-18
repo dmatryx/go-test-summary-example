88cdebbb16983d9ccde597568ad810a1499968b3
Greg Sweetman <krypton@matryx.org.uk>
2023-12-18 13:44:47 +0000

    Remove failing tests to demonstrate success

--- internal/example/example_test.go
+++ internal/example/example_test.go
@@ -28,31 +28,6 @@ func TestAddNumbers(t *testing.T) {
 	}
 }
 
-func TestAddNumbersAgainButFailTwotests(t *testing.T) {
-	type testCase struct {
-		name   string
-		value1 int
-		value2 int
-		result int
-	}
-	testCases := []testCase{
-		{"1+1", 1, 1, 2},
-		{"1+2", 1, 2, 5},
-		{"2+1", 2, 1, 5},
-		{"5+0", 5, 0, 5},
-		{"0+0", 0, 0, 0},
-		{"0+0", 0, 0, 0},
-		{"0+0", 0, 0, 0},
-		{"0+0", 0, 0, 0},
-		{"-1+-1", -1, -1, -2},
-	}
-	for _, test := range testCases {
-		t.Run(test.name, func(t *testing.T) {
-			assert.True(t, AddNumbers(test.value1, test.value2) == test.result)
-		})
-	}
-}
-
 func TestMirrorString(t *testing.T) {
 	assert.True(t, MirrorString("test") == "test")
 }
